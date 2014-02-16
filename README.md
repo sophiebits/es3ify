987c26863aa6c47b20005cefc49af6625076c582
Ben Alpert <spicyjalapeno@gmail.com>
2014-02-15 16:29:32 -0800

    Strip trailing commas from arrays and objects

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # es3ify
 
-Browserify transform to convert quote reserved words in property keys for compatibility with ES3 JavaScript engines like IE8.
+Browserify transform to convert quote reserved words in property keys for compatibility with ES3 JavaScript engines like IE8. In addition, trailing commas in array and object literals are removed.
 
 ``` javascript
 // In
-var x = {class: 2};
-x.class = 3;
+var x = {class: 2,};
+x.class = [3, 4,];
 
 // Out:
 var x = {"class": 2};
-x["class"] = 3;
+x["class"] = [3, 4];
 ```
--- index.js
+++ index.js
@@ -18,13 +18,17 @@ reserved.forEach(function(k) {
     reservedDict[k] = true;
 });
 
+// In: x.class = 3;
+// Out: x["class"] = 3;
 function visitMemberExpression(traverse, node, path, state) {
+    traverse(node.object, path, state);
     utils.catchup(node.object.range[1], state);
     utils.append('[', state);
     utils.catchupWhiteSpace(node.property.range[0], state);
     utils.append('"', state);
     utils.catchup(node.property.range[1], state);
     utils.append('"]', state);
+    return false;
 }
 visitMemberExpression.test = function(node, path, state) {
     return node.type === Syntax.MemberExpression &&
@@ -32,11 +36,16 @@ visitMemberExpression.test = function(node, path, state) {
         reservedDict[node.property.name] === true;
 };
 
+// In: x = {class: 2};
+// Out: x = {"class": 2};
 function visitProperty(traverse, node, path, state) {
     utils.catchup(node.key.range[0], state);
     utils.append('"', state);
     utils.catchup(node.key.range[1], state);
     utils.append('"', state);
+    utils.catchup(node.value.range[0], state);
+    traverse(node.value, path, state);
+    return false;
 }
 visitProperty.test = function(node, path, state) {
     return node.type === Syntax.Property &&
@@ -44,19 +53,74 @@ visitProperty.test = function(node, path, state) {
         reservedDict[node.key.name] === true;
 };
 
-var visitorList = [visitMemberExpression, visitProperty];
+var reCommaOrComment = /,|\/\*.+?\*\/|\/\/[^\n]+/g;
+function stripComma(value) {
+  return value.replace(reCommaOrComment, function(text) {
+    if (text === ',') {
+        return '';
+    } else {
+        // Preserve comments
+        return text;
+    }
+  });
+}
 
-module.exports = function(file) {
+// In: [1, 2, 3,]
+// Out: [1, 2, 3]
+function visitArrayOrObjectExpression(traverse, node, path, state) {
+    // Copy the opening '[' or '{'
+    utils.catchup(node.range[0] + 1, state);
+
+    var elements = node.type === Syntax.ArrayExpression ?
+        node.elements :
+        node.properties;
+    elements.forEach(function(element, i) {
+        if (element == null && i === elements.length - 1) {
+            throw new Error(
+                "Elisions ending an array are interpreted inconsistently " +
+                "in IE8; remove the extra comma or use 'undefined' explicitly");
+        }
+        if (element != null) {
+            // Copy commas from after previous element, if any
+            utils.catchup(element.range[0], state);
+            traverse(element, path, state);
+        }
+    });
+
+    // Skip over a trailing comma, if any
+    utils.catchup(node.range[1] - 1, state, stripComma);
+    utils.catchup(node.range[1], state);
+    return false;
+}
+visitArrayOrObjectExpression.test = function(node, path, state) {
+    return node.type === Syntax.ArrayExpression ||
+        node.type === Syntax.ObjectExpression;
+};
+
+var visitorList = [
+    visitMemberExpression,
+    visitProperty,
+    visitArrayOrObjectExpression
+];
+
+function transform(code) {
+    return jstransform.transform(visitorList, code).code;
+}
+
+function process(file) {
     var data = '';
     function write(chunk) {
         data += chunk;
     }
 
     function compile() {
-        var transformed = jstransform.transform(visitorList, data);
-        this.queue(transformed.code);
+        this.queue(transform(data));
         this.queue(null);
     }
 
     return through(write, compile);
-};
+}
+
+module.exports = process;
+module.exports.transform = transform;
+module.exports.visitorList = visitorList;
