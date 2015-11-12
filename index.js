52604cb253e4ffdfdd7c5ae435888352f219573d
Kevin Wenner <kevin.wenner@massmotionmedia.com>
2015-11-12 14:41:26 +0100

    transform js files only

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ function transform(code) {
 }
 
 function process(file) {
-    if (/\.json$/.test(file)) return through();
+    if (!/\.js$/.test(file)) return through();
     var data = '';
     function write(chunk) {
         data += chunk;
